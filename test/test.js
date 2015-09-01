29babb01a3f7e34e013c08eac21b5c861c95ecf5
Malcolm Dwyer <malcolm.dwyer@gmail.com>
2015-08-31 22:26:16 -0500

    Added 'defined' op with tests

--- test/test.js
+++ test/test.js
@@ -10,16 +10,19 @@ describe('json-predicate', function() {
   before(function() {
     in0 = {
       num1: 1,
+      null1: null,
       stringA: 'A',
       stringABC: 'ABC',
       stringABC_123: 'ABC_123',
       objA: {
         num2: 2,
+        null2: null,
         stringX: 'X',
         stringXYZ: 'XYZ',
         stringXYZ_789: 'XYZ_789',
         objB: {
           num3: 3,
+          null3: null,
           stringM: 'M',
           stringMNO: 'MNO',
           stringMNO_456: 'MNO_456'
@@ -36,6 +39,16 @@ describe('json-predicate', function() {
 
   it('should return false for empty predicate', function() {
     pred = {};
+    result = test(in0, pred);
+    result.should.be.false;
+  });
+
+  it('rejects unknown operations', function() {
+    pred = {
+      op: 'not_an_op',
+      path: '/num1',
+      value: 1
+    }
     result = test(in0, pred);
     result.should.be.false;
   });
@@ -69,6 +82,50 @@ describe('json-predicate', function() {
         pred.value = 'AB';
         pred.path = '/objA/stringXYZ';
         result = test(in0, pred)
+        result.should.be.false;
+      });
+    });
+
+    describe('defined operation', function() {
+      beforeEach(function() {
+        pred = {
+          op: 'defined'
+        };
+      });
+
+      it('returns true for existing key (shallow path)', function() {
+        pred.path = '/num1';
+        result = test(in0, pred);
+        result.should.be.true;
+      });
+
+      it('returns true for existing key with null value (shallow path)', function() {
+        pred.path = '/null1';
+        result = test(in0, pred);
+        result.should.be.true;
+      });
+
+      it('returns false for non-existent key (shallow path)', function() {
+        pred.path = '/not_a_key';
+        result = test(in0, pred);
+        result.should.be.false;
+      });
+
+      it('returns true for existing key (deep path)', function() {
+        pred.path = '/objA/objB/num3';
+        result = test(in0, pred);
+        result.should.be.true;
+      });
+
+      it('returns true for existing key with null value (deep path)', function() {
+        pred.path = '/objA/objB/null3';
+        result = test(in0, pred);
+        result.should.be.true;
+      });
+
+      it('returns false for non-existent key (deep path)', function() {
+        pred.path = '/objA/objB/not_a_key';
+        result = test(in0, pred);
         result.should.be.false;
       });
     });
